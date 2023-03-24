aea40c9ef7a9ca907ecf888c231708cd2f260732
fzw1202 <joe_1202@126.com>
2023-03-24 20:10:28 +0800

    Add keyboard features, fix bugs Write README document

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # web-calculator
-a simple JavaScript homework
+A simple JavaScript homework
+
+
+### Features:
+- have one register for calculation
+- support addition
+- press AC to clear the screen and the register
+- press CE to only clear the screen and input numbers again
+- support keyboard input
+  - press 0 ~ 9 for numbers
+  - press + for add
+  - press = for equal
+  - press backspace or delete for deleting number
+  - press escape for performing AC
+- carefully managed the issues of button pressing order
+- try to simulate the output of iphone calculator(under some undefine events)
+
+
+### TODO
+- support minus, plus and divide
+- take care of data overflow
+- add press animation for buttons
--- js/calc.js
+++ js/calc.js
@@ -8,115 +8,171 @@ let fn7 = document.querySelector("#seven");
 let fn8 = document.querySelector("#eight");
 let fn9 = document.querySelector("#nine");
 let fn0 = document.querySelector("#zero");
-let fnPlus = document.querySelector("#plus");
+let fnAdd = document.querySelector("#add");
 let fnEqual = document.querySelector("#equal");
 let fnAC = document.querySelector("#AC");
 let fnCE = document.querySelector("#CE");
 
 let screen = document.querySelector("#screen");
 let result = 0, add = 0;
-let isEqual = 0, isPlus = 0, isValid = 0;
+let isEqual = 0, isAdd = 0, isValid = 0, isContinue = 0;
 
 fn1.onclick = function () {
-    if (isPlus)
+    if (isEqual) {
+        screen.textContent = "0";
+        result = add;
+        isContinue = 1;
+        console.log(result);
+    }
+    if (isAdd)
         screen.textContent = "0";
     screen.textContent = parseInt(screen.textContent + "1");
     isEqual = 0;
-    isPlus = 0;
+    isAdd = 0;
 }
 
 fn2.onclick = function () {
-    if (isPlus)
+    if (isEqual) {
+        screen.textContent = "0";
+        result = add;
+        isContinue = 1;
+    }
+    if (isAdd)
         screen.textContent = "0";
     screen.textContent = parseInt(screen.textContent + "2");
     isEqual = 0;
-    isPlus = 0;
+    isAdd = 0;
 }
 
 fn3.onclick = function () {
-    if (isPlus)
+    if (isEqual) {
+        screen.textContent = "0";
+        result = add;
+        isContinue = 1;
+    }
+    if (isAdd)
         screen.textContent = "0";
     screen.textContent = parseInt(screen.textContent + "3");
     isEqual = 0;
-    isPlus = 0;
+    isAdd = 0;
 }
 
 fn4.onclick = function () {
-    if (isPlus)
+    if (isEqual) {
+        screen.textContent = "0";
+        result = add;
+        isContinue = 1;
+    }
+    if (isAdd)
         screen.textContent = "0";
     screen.textContent = parseInt(screen.textContent + "4");
     isEqual = 0;
-    isPlus = 0;
+    isAdd = 0;
 }
 
 fn5.onclick = function () {
-    if (isPlus)
+    if (isEqual) {
+        screen.textContent = "0";
+        result = add;
+        isContinue = 1;
+    }
+    if (isAdd)
         screen.textContent = "0";
     screen.textContent = parseInt(screen.textContent + "5");
     isEqual = 0;
-    isPlus = 0;
+    isAdd = 0;
 }
 
 fn6.onclick = function () {
-    if (isPlus)
+    if (isEqual) {
+        screen.textContent = "0";
+        result = add;
+        isContinue = 1;
+    }
+    if (isAdd)
         screen.textContent = "0";
     screen.textContent = parseInt(screen.textContent + "6");
     isEqual = 0;
-    isPlus = 0;
+    isAdd = 0;
 }
 
 fn7.onclick = function () {
-    if (isPlus)
+    if (isEqual) {
+        screen.textContent = "0";
+        result = add;
+        isContinue = 1;
+    }
+    if (isAdd)
         screen.textContent = "0";
     screen.textContent = parseInt(screen.textContent + "7");
     isEqual = 0;
-    isPlus = 0;
+    isAdd = 0;
 }
 
 fn8.onclick = function () {
-    if (isPlus)
+    if (isEqual) {
+        screen.textContent = "0";
+        result = add;
+        isContinue = 1;
+    }
+    if (isAdd)
         screen.textContent = "0";
     screen.textContent = parseInt(screen.textContent + "8");
     isEqual = 0;
-    isPlus = 0;
+    isAdd = 0;
 }
 
 fn9.onclick = function () {
-    if (isPlus)
+    if (isEqual) {
+        screen.textContent = "0";
+        result = add;
+        isContinue = 1;
+    }
+    if (isAdd)
         screen.textContent = "0";
     screen.textContent = parseInt(screen.textContent + "9");
     isEqual = 0;
-    isPlus = 0;
+    isAdd = 0;
 }
 
 fn0.onclick = function () {
-    if (isPlus)
+    if (isEqual) {
+        screen.textContent = "0";
+        result = add;
+        isContinue = 1;
+    }
+    if (isAdd)
         screen.textContent = "0";
     screen.textContent = parseInt(screen.textContent + "0");
     isEqual = 0;
-    isPlus = 0;
+    isAdd = 0;
 }
 
-fnPlus.onclick = function () {
-    if (!isEqual && !isPlus) {
+fnAdd.onclick = function () {
+    if (!isEqual && !isAdd) {
         add = parseInt(screen.textContent);
         // console.log(add);
         result += add;
     }
     isEqual = 0;
-    isPlus = 1;
+    isAdd = 1;
     isValid = 1;
 }
 
 fnEqual.onclick = function () {
     if (isValid) {
-        if (!isEqual)
+        if (!isEqual) {
             add = parseInt(screen.textContent);
+            if (isContinue) {
+                [add, result] = [result, add];
+                isContinue = 0;
+            }
+        }
         result += add;
         // console.log(result);
         screen.textContent = result;
         isEqual = 1;
-        isPlus = 0;
+        isAdd = 0;
     }
 }
 
@@ -126,12 +182,38 @@ fnAC.onclick = function () {
     result = 0;
     add = 0;
     isEqual = 0;
-    isPlus = 0;
+    isAdd = 0;
     isValid = 0;
 }
 
 fnCE.onclick = function () {
-    if (!isEqual && !isPlus)
+    if (!isEqual && !isAdd)
         screen.textContent = "0";
 }
 
+document.onkeydown = function(event){
+    var code = event.keyCode;
+    if (code == 8 || code == 46) {
+        screen.textContent = screen.textContent.slice(0, -1);
+        if (screen.textContent == "")
+            screen.textContent = "0";
+    }
+    if (code >= 48 && code <= 57) {
+        if (isEqual) {
+            screen.textContent = "0";
+            result = add;
+            isContinue = 1;
+        }
+        if (isAdd)
+            screen.textContent =  "0";
+        screen.textContent = parseInt(screen.textContent + (code - 48));
+        isEqual = 0;
+        isAdd = 0;
+    }
+    if ((!event.shiftKey && code == 187) || code == 13)  // equal
+        fnEqual.onclick();
+    if (code == 107 || (event.shiftKey && code == 187))  // add
+        fnAdd.onclick();
+    if (code == 27)  // esc
+        fnAC.onclick();
+}
